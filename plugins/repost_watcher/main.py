3666041245181675eb3e525335312bd7eaf5cd7c
fazilfazfaz <fazilchr@gmail.com>
2023-11-16 20:38:40 +0100

    * Remove discord parsing of urls in the repost watcher

--- plugins/repost_watcher/main.py
+++ plugins/repost_watcher/main.py
@@ -1,33 +1,23 @@
+import re
+
 from database.helper import gfd_links_database_helper
 from database.models import PostedLink
 from plugins.base import BasePlugin
 
 
 class RepostWatcher(BasePlugin):
-    number_emoji_map = {
-        0: '0️⃣',
-        1: '1️⃣',
-        2: '2️⃣',
-        3: '3️⃣',
-        4: '4️⃣',
-        5: '5️⃣',
-        6: '6️⃣',
-        7: '7️⃣',
-        8: '8️⃣',
-        9: '9️⃣',
-    }
+    basic_url_regex_pattern = re.compile(r'https?://[^\s]{1,2048}', re.DOTALL)
 
     async def on_message(self, message):
         if message.content == '.toplinks':
             await self.post_top_links(message)
             return
         reacted = False
-        for embed in message.embeds:
-            if embed.url != '':
-                posted_link = await self.process_link(embed.url)
-                if reacted is False and posted_link.hits > 1:
-                    reacted = True
-                    await message.add_reaction('♻')
+        for link in re.finditer(self.basic_url_regex_pattern, message.content):
+            posted_link = await self.process_link(link.group(0))
+            if reacted is False and posted_link.hits > 1:
+                reacted = True
+                await message.add_reaction('♻')
 
     @staticmethod
     async def process_link(link) -> PostedLink:
@@ -39,14 +29,8 @@ class RepostWatcher(BasePlugin):
         gfd_links_database_helper.release_db()
         return posted_link
 
-    def emoji_numbers_for_hits(self, hits):
-        if hits < 10:
-            yield self.number_emoji_map[hits]
-            return
-        yield from self.emoji_numbers_for_hits(hits // 10)
-        yield self.number_emoji_map[hits % 10]
-
-    async def post_top_links(self, message):
+    @staticmethod
+    async def post_top_links(message):
         gfd_links_database_helper.replenish_db()
         num_links = 10
         posted_links: list[PostedLink] = PostedLink.get_top_links(num_links)
