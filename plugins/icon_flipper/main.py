81e0a89c8fe4a89077639bc1de60fa5a310b9a8f
fazilfazfaz <fazilchr@gmail.com>
2024-04-06 18:20:52 +0200

    * Update the timing mechanism of the icon flipper

--- plugins/icon_flipper/main.py
+++ plugins/icon_flipper/main.py
@@ -1,6 +1,6 @@
 import asyncio
+import datetime
 import io
-from random import randrange
 
 import discord
 from PIL import Image
@@ -9,41 +9,32 @@ from plugins.base import BasePlugin
 
 
 class IconFlipper(BasePlugin):
-    icon_flipper_hours_min: int
-    icon_flipper_hours_max: int
-    icon_flipper_minutes_min: int
-    icon_flipper_minutes_max: int
+    icon_flipper_hours_start: int
+    icon_flipper_hours_end: int
 
-    def on_ready(self):
+    async def on_ready(self):
         if self.is_ready():
             return
-        if 'ICON_FLIPPER_HOURS_MIN' in self.config:
-            self.icon_flipper_hours_min = int(self.config['ICON_FLIPPER_HOURS_MIN'])
+        if 'ICON_FLIPPER_HOURS_START' in self.config:
+            self.icon_flipper_hours_start = int(self.config['ICON_FLIPPER_HOURS_START'])
         else:
             return
-        if 'ICON_FLIPPER_HOURS_MAX' in self.config:
-            self.icon_flipper_hours_max = int(self.config['ICON_FLIPPER_HOURS_MAX'])
+        if 'ICON_FLIPPER_HOURS_END' in self.config:
+            self.icon_flipper_hours_end = int(self.config['ICON_FLIPPER_HOURS_END'])
         else:
             return
-        if 'ICON_FLIPPED_MINUTES_MIN' in self.config:
-            self.icon_flipper_minutes_min = int(self.config['ICON_FLIPPED_MINUTES_MIN'])
-        else:
-            return
-        if 'ICON_FLIPPED_MINUTES_MAX' in self.config:
-            self.icon_flipper_minutes_max = int(self.config['ICON_FLIPPED_MINUTES_MAX'])
-        else:
-            return
-        asyncio.get_event_loop().create_task(self.run())
+        await self.flip_channel_icon()
+        # asyncio.get_event_loop().create_task(self.run())
 
     async def run(self):
         while True:
             try:
-                sleep_seconds = randrange(self.icon_flipper_hours_min, self.icon_flipper_hours_max) * 60 * 60
+                sleep_seconds = self.get_seconds_until_hour(self.icon_flipper_hours_start)
                 print(f'Waiting {sleep_seconds} to flip the image')
                 await asyncio.sleep(sleep_seconds)
                 print('Flipping the channel icon')
                 await self.flip_channel_icon()
-                sleep_seconds = randrange(self.icon_flipper_minutes_min, self.icon_flipper_minutes_max) * 60
+                sleep_seconds = self.get_seconds_until_hour(self.icon_flipper_hours_end)
                 print(f'Waiting {sleep_seconds} to flip the image back')
                 await asyncio.sleep(sleep_seconds)
                 print('Flipping the channel icon back')
@@ -62,3 +53,11 @@ class IconFlipper(BasePlugin):
         icon_image_flipped.save(icon_bytes_flipped, icon_image.format)
         icon_bytes_flipped.seek(0)
         await guild.edit(icon=icon_bytes_flipped.read())
+
+    @staticmethod
+    def get_seconds_until_hour(hour):
+        now = datetime.datetime.now()
+        day_delta = datetime.timedelta(hours=24)
+        target_hour = now.replace(hour=hour, minute=0, second=0, microsecond=0)
+        target_delta = day_delta - (now - target_hour)
+        return target_delta.total_seconds() % (24 * 3600)
