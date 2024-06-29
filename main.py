f311029de26ff81575bdf9d6e084638d5aa86d13
fazilfazfaz <fazilchr@gmail.com>
2024-06-29 15:50:46 +0200

    * Stop using custom bot user id env var

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.id == int(config['BOT_USER_ID']):
+    if message.author.id == client.user.id:
         return
     await smoothie_maker.on_message(message)
     await comment_hearter.on_message(message)
